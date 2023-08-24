016c43f9c76a1e989c43fa3bb2ffc751d7fcb738
thaiducpro <nguyenhuuthai.pro@gmail.com>
2023-08-25 01:29:44 +1000

    Initial commit

---
+++ lib/price_screen.dart
@@ -0,0 +1,100 @@
+import 'dart:io' show Platform;
+
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = "USD";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: getPicker()),
+        ],
+      ),
+    );
+  }
+
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      return getIOSPicker();
+    } else {
+      return getAndroidDropdown();
+    }
+  }
+
+  CupertinoPicker getIOSPicker() {
+    List<Text> items = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      items.add(Text(currenciesList[i]));
+    }
+    return CupertinoPicker(
+        itemExtent: 32,
+        onSelectedItemChanged: (selectedIndex) {
+          print(selectedIndex);
+        },
+        children: items);
+  }
+
+  DropdownButton<String> getAndroidDropdown() {
+    List<DropdownMenuItem<String>> items = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      DropdownMenuItem<String> item = DropdownMenuItem(
+        child: Text(currenciesList[i]),
+        value: currenciesList[i],
+      );
+      items.add(item);
+    }
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: items,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedCurrency = value!;
+            print(value);
+          },
+        );
+      },
+    );
+  }
+}
